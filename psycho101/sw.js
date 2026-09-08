d5072b48234d2f34945f76bb99f24345d353413d
itayduv-arch <itayduv@gmail.com>
2026-09-08 21:55:21 +0300

    Refresh cache for RTL math fix

--- psycho101/sw.js
+++ psycho101/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='idpsycho101-v12';
+const CACHE_NAME='idpsycho101-v13';
 const CORE=['./','./index.html','./version.json','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
