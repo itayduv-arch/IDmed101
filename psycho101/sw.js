357b0491150fd660a27ee6a1182e1829ed97ca8e
itayduv-arch <itayduv@gmail.com>
2026-09-08 23:01:35 +0300

    Refresh IDpsycho101 cache for friendly UI

--- psycho101/sw.js
+++ psycho101/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='idpsycho101-v14';
+const CACHE_NAME='idpsycho101-v16';
 const CORE=['./','./index.html','./version.json','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
