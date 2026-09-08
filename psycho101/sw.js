0c522e7beffedfd685381971ed1d9bf1647bbe87
itayduv-arch <itayduv@gmail.com>
2026-09-08 21:05:21 +0300

    Refresh IDpsycho101 cache for Campus IL build

--- psycho101/sw.js
+++ psycho101/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='idpsycho101-v8';
+const CACHE_NAME='idpsycho101-v9';
 const CORE=['./','./index.html','./version.json','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
