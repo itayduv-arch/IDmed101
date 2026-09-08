c1bcd6c632f1b1ce6b5a50eb9e70ee24083128e7
itayduv-arch <itayduv@gmail.com>
2026-09-08 21:40:22 +0300

    Refresh cache for Duolingo-style build

--- psycho101/sw.js
+++ psycho101/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='idpsycho101-v9';
+const CACHE_NAME='idpsycho101-v11';
 const CORE=['./','./index.html','./version.json','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
