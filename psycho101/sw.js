09fa05a600256ac703c52e59238d6d507280268e
itayduv-arch <itayduv@gmail.com>
2026-09-22 18:29:06 +0300

    Cache advanced Psycho101 bank offline

--- psycho101/sw.js
+++ psycho101/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='idpsycho101-v28';
-const CORE=['./','./index.html','./version.json','./manifest.webmanifest','./official_quant_expansion.js','./daily_expansion_20260918.js'];
+const CACHE_NAME='idpsycho101-v29';
+const CORE=['./','./index.html','./version.json','./manifest.webmanifest','./official_quant_expansion.js','./daily_expansion_20260918.js','./advanced_expansion_20260922.js'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
